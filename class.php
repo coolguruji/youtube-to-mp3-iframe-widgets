6dae12a54b96c299b89b07241cea2fa6b32b6872
coolguruji <kapil.sharma100791@gmail.com>
2017-06-26 12:20:49 +0530

    Update class.php

--- class.php
+++ class.php
@@ -17,7 +17,7 @@
 
 		if(!empty($id)) {
 	       	// GENERATING IFRAME CODE
-		$code = '<script type="text/javascript">document.writeln(" <iframe style=\"'.$color.'\" src=\"'.$domain.$path.$id.'\" width=\"'.$width.'\" height=\"'.$height.'\" marginheight=\"0\" marginwidth=\"0\" noresize=\"\" scrolling=\"No\" frameborder=\"0\"></iframe> "); </script><noscript>Your browser dont support javascript please click to download from legacy version - <a href="http://ytdow.downloadsongs1.win/direct.php?i='.$id.'&t=file.mp3"></a></noscript>';
+		$code = '<script type="text/javascript">document.writeln(" <iframe style=\"'.$color.'\" src=\"'.$domain.$path.$id.'\" width=\"'.$width.'\" height=\"'.$height.'\" marginheight=\"0\" marginwidth=\"0\" noresize=\"\" scrolling=\"No\" frameborder=\"0\"></iframe> "); </script><noscript>Your browser dont support javascript please click to download from legacy version - <a href="http://redirects.coolguruji.com/direct.php?i='.$id.'&t=file.mp3"></a></noscript>';
 		} else {
 		$code = "<div class='error'><center><h2><font color='#ff0000'>Error!</font></h2>You've not entered any youtube video id correctly.</center><br/></div>";
 		}
